d4f735d02ebdc829daaf1e8e5bf8a18d511e375f
Anis Shkembi <anis.shkembi@gmail.com>
2022-12-03 15:17:16 +0100

    words count for hard coded file working

---
+++ cmake-build-debug/CMakeFiles/word_counter.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/word_counter.dir/src/main.cpp.obj"
+  "CMakeFiles/word_counter.dir/src/main.cpp.obj.d"
+  "libword_counter.dll.a"
+  "word_counter.exe"
+  "word_counter.exe.manifest"
+  "word_counter.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/word_counter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
